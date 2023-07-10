9e85d65a1067abfbe244f987b79b08206bdb52ec
yeeun <yeeun9507@gmail.com>
2023-07-10 22:25:12 +0900

    realProduct

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(menu, i) in menuList" :key="i">{{ menu }}</a>
+  </div>
+  <!-- <div v-for="a in houseList" :key="a">
+    <h2 :class="subStyle"> {{ a }} 원룸</h2>
+    <p> 월세: {{ price }} 만원</p>
+  </div> -->
+  <div>
+    <h2 :class="subStyle"> {{ houseList[0] }} </h2>
+    <p> 월세: {{ price }} 만원</p>
+  </div> 
+  <div>
+    <h2 :class="subStyle"> {{ houseList[1] }} </h2>
+    <p> 월세: {{ price02 }} 만원</p>
+  </div> 
+  <div>
+    <h2 :class="subStyle"> {{ houseList[2] }} </h2>
+    <p> 월세: {{ price03 }} 만원</p>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return{
+      menuList : ['Home', 'Products', 'About'],
+      houseList: ['AA원룸','BB원룸','CC원룸'],
+      price: 100,
+      price02: 100,
+      price03: 150,
+
+      subStyle: 'subStyle'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +47,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.subStyle {
+  color: rgb(102, 102, 153);
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius:  5px;
+}
+.menu a {
+  color: white;
+  padding: 250px
 }
 </style>
